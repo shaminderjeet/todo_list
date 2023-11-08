2278553ace007afe788b34fa8f5e0705cb348726
Shaminderjeet kaur <shaminderjeet92@gmail.com>
2023-11-08 13:00:14 +0100

    added gitignore to ignore specific files

--- todo.js
+++ todo.js
@@ -4,10 +4,9 @@ const args = process.argv;
 const readTasks = () => {
   const input = fs.readFileSync("tasks.txt");
   let tasks = input.toString().split("\r\n");
-  // console.log(tasks)
+
   let nonEmptyTasks = tasks.filter((element) => element !== "");
 
-  console.log({ tasks, nonEmptyTasks });
   return nonEmptyTasks;
 };
 
@@ -20,15 +19,13 @@ const printTasks = (tasks) => {
     console.log(`[ ] ${element} `);
   });
 };
-let command=args[args.length - 2] 
-let newTask=args[args.length - 1]
-if (command=== "add") {
+let command = args[args.length - 2];
+let newTask = args[args.length - 1];
+if (command === "add") {
   const tasks = readTasks();
-  console.log("read", tasks);
   tasks.push(newTask);
   console.log(`"${newTask}" added (${tasks.length} task to do:)`);
   writeTasks(tasks);
-  console.log("write", tasks);
 } else {
   const tasks = readTasks();
   console.log(`${tasks.length} tasks to do:`);
